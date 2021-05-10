cc581749190041dd720fdc66f753a00853958bd0
kevinmainairungu <mainairungu99@gmail.com>
2021-05-10 12:32:52 +0300

    Initial commit

---
+++ app/src/main/java/com/raywenderlich/kevin/timefighter/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.raywenderlich.kevin.timefighter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
